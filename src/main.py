e5f1a8bd3c5d14b6bdf5644c063ebd237918048d
Polina <kuranovapolina@gmail.com>
2024-11-21 18:58:18 +0300

    callback template

--- src/main.py
+++ src/main.py
@@ -4,15 +4,37 @@ import matplotlib.pyplot as plt
 import time
 from lxml import etree # lxml is needed to change .xml models in script
 
-# model = mujoco.MjModel.from_xml_path("/Users/polinakuranova/uni/robot_simulation/robot_simulation/course_work/models/differentialdrive.xml")
-model = mujoco.MjModel.from_xml_path("/Users/polinakuranova/uni/robot_simulation/robot_simulation/course_work/models/car.xml")
+model = mujoco.MjModel.from_xml_path("../models/project.xml")
 data = mujoco.MjData(model)
 
-# print(data.ctrl)
+start_time = 0
 
-with mujoco.viewer.launch_passive(model, data) as viewer:
-  data.ctrl = [50., 0.1]
+def control_func(model, data):
+  if step_start - start_time < 3:
+    data.ctrl = [0, 0, 0.0, 0, 0, 0, 0]
+  elif step_start - start_time < 4:
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif step_start - start_time < 5.5:
+    data.ctrl = [0, 0, 0.1, 0.1, 0, 0, 0]
+  elif step_start - start_time < 7:
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  # elif step_start - start_time < 8:
+  #   data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  else:
+    data.ctrl = [0, 0, 0, 0, 0, 0, 0]
+
+with mujoco.viewer.launch_passive(model, data) as viewer:  
+  start_time = time.time()
+  step_start = start_time
+
+  # mujoco.set_mjcb_control(control_func)
+
   while viewer.is_running():
+    step_start = time.time()
+    x = data.body("car").xpos
+
+    print(step_start - start_time, x)
+
     mujoco.mj_step(model, data)
     viewer.sync()
  
