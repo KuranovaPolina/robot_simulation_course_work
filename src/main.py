8914c960cc121ba78fd0228b7a3132220a412b85
Polina <kuranovapolina@gmail.com>
2024-11-22 20:33:34 +0300

    8 route

--- src/main.py
+++ src/main.py
@@ -3,37 +3,90 @@ import mujoco.viewer
 import matplotlib.pyplot as plt
 import time
 from lxml import etree # lxml is needed to change .xml models in script
+import math
 
-model = mujoco.MjModel.from_xml_path("../models/project.xml")
+model = mujoco.MjModel.from_xml_path("../models/project_parallells.xml")
 data = mujoco.MjData(model)
 
 start_time = 0
 
-def control_func(model, data):
-  if step_start - start_time < 3:
+def control_func_1(model, data):
+  l_time = 0.2
+  r_time = l_time * math.pi / 4 + 0.1
+  wait_time = 3
+  time_part = (step_start - start_time) % (wait_time + 8 * l_time + 6 * r_time)
+
+  if (time_part) < wait_time:
     data.ctrl = [0, 0, 0.0, 0, 0, 0, 0]
-  elif step_start - start_time < 4:
+  elif (time_part) < (wait_time + l_time):
     data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
-  elif step_start - start_time < 5.5:
-    data.ctrl = [0, 0, 0.1, 0.1, 0, 0, 0]
-  elif step_start - start_time < 7:
+  elif (time_part) < (wait_time + l_time + r_time):
+    data.ctrl = [0, 0, 0.1, 0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 2 * l_time + r_time):
     data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
-  # elif step_start - start_time < 8:
-  #   data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 2 * l_time + 2 * r_time):
+    data.ctrl = [0, 0, 0.1, 0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 3 * l_time + 2 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 3 * l_time + 3 * r_time):
+    data.ctrl = [0, 0, 0.1, 0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 4 * l_time + 3 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 5 * l_time + 3 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 5 * l_time + 4 * r_time):
+    data.ctrl = [0, 0, 0.1, -0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 6 * l_time + 4 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 6 * l_time + 5 * r_time):
+    data.ctrl = [0, 0, 0.1, -0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 7 * l_time + 5 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 7 * l_time + 6 * r_time):
+    data.ctrl = [0, 0, 0.1, -0.7, 0, 0, 0]
+  elif (time_part) < (wait_time + 8 * l_time + 6 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
   else:
     data.ctrl = [0, 0, 0, 0, 0, 0, 0]
+    # data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+
+def control_func_2(model, data):
+  l_time = 0.2
+  r_time = l_time * math.pi / 2
+  wait_time = 3
+  time_part = (step_start - start_time) % (wait_time + 8 * l_time + 4 * r_time)
+
+  if (time_part) < wait_time:
+    data.ctrl = [0, 0, 0.0, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + l_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + l_time + r_time):
+    data.ctrl = [0, 0, 0.1, 0.9, 0, 0, 0]
+  elif (time_part) < (wait_time + 2 * l_time + r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 3 * l_time + r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 3 * l_time + 2 * r_time):
+    data.ctrl = [0, 0, 0.1, -0.9, 0, 0, 0]
+  elif (time_part) < (wait_time + 4 * l_time + 2 * r_time):
+    data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
+  elif (time_part) < (wait_time + 4 * l_time + 12 * r_time):
+    data.ctrl = [0, 0, 0.05, -0.2, 0, 0, 0]
+  else:
+    data.ctrl = [0, 0, 0, 0, 0, 0, 0]
+    # data.ctrl = [0, 0, 0.1, 0, 0, 0, 0]
 
 with mujoco.viewer.launch_passive(model, data) as viewer:  
   start_time = time.time()
   step_start = start_time
 
-  # mujoco.set_mjcb_control(control_func)
+  mujoco.set_mjcb_control(control_func_2)
 
   while viewer.is_running():
     step_start = time.time()
     x = data.body("car").xpos
 
-    print(step_start - start_time, x)
+    # print(step_start - start_time, x)
 
     mujoco.mj_step(model, data)
     viewer.sync()
